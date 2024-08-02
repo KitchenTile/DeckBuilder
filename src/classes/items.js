6bc35532d7cb46e6fb8291fb65fb12fee4f83715
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-02 23:51:00 +0100

    Add files via upload

---
+++ src/classes/items.js
@@ -0,0 +1,11 @@
+export default class item{
+    constructor(name, effect, description, symbol) {
+        this.name = name;
+        this.effect = effect;
+        this.description = description;
+        this.symbol = symbol;
+    }
+    
+
+
+}
