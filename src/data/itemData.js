0aebb9a0c9f8b241d3cc37bed99603cc87e3ccea
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-04 00:30:20 +0100

    Add files via upload

--- src/data/itemData.js
+++ src/data/itemData.js
@@ -1,5 +1,10 @@
+
 const itemData = [
-    {name: "ITEM 1", effect: 25, description: "permanently increases health by 25HP", symbol: ""}
+    {name: "ITEM 1", effect: 25, description: "Permanently increases health by 25HP", symbol: ""},
+    {name: "ITEM 2", effect: 1, description: "Permanently increases energy by 1", symbol: ""}
+
 ];
+
+
 
 export default itemData;
