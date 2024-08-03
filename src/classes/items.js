c64c3e0a46ca82aedf8e4a8a024f169aff2a5e35
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-03 10:09:38 +0100

    Add files via upload

--- src/classes/items.js
+++ src/classes/items.js
@@ -1,11 +1,25 @@
-export default class item{
+export class item{
     constructor(name, effect, description, symbol) {
         this.name = name;
         this.effect = effect;
         this.description = description;
         this.symbol = symbol;
     }
+
+
     
 
+}
 
+export const createItem = (itemInfo) => { // function that creates items from data
+    return new item(itemInfo.name, itemInfo.effect, itemInfo.description, itemInfo.symbol);
+}
+
+export const rewardItem = (itemData) => {
+    const itemList = [];
+    itemData.forEach(item => {
+        itemList.push(createItem(item));
+    });
+
+    console.log(itemList);
 }
---
+++ src/data/itemData.js
@@ -0,0 +1,5 @@
+const itemData = [
+    {name: "ITEM 1", effect: 25, description: "", symbol: ""}
+];
+
+export default itemData;
