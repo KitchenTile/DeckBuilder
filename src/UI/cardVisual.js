87d87809a34c629128feebb26bbb87d6cdb31a13
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-11 21:18:19 +0100

    Add files via upload

--- src/UI/cardVisual.js
+++ src/UI/cardVisual.js
@@ -26,7 +26,32 @@ const displayCard = (hand) => {
         </div>
         `
     });
-    
 }
 
-export default displayCard;
+const displayRewardCard = (rewardList) => {
+    const cardRewardVisual = document.getElementById("card_Reward_Visual");
+    cardRewardVisual.innerHTML = ""; // Clear the container to make make a new one for next card
+
+
+    /* it's the same as the function above, I feel like it should just be one to avoid code dupping. I also had to change the id name of each card in te container*/
+    rewardList.forEach((card, index) => {
+        cardRewardVisual.innerHTML += `
+        <div class="card_container ${card.type}" id="reward_card_${index}">
+            <div class="name_symbol">
+                <h1>${card.title} </h1>
+                <h1>${card.symbol}</h1>
+            </div>
+            <img src= ${card.img} alt="img" class="img"/>
+            <div class="effect_energy">
+                <h3 class="effect">value: ${card.effect}</h3>
+                <h3>Cost: ${card.energyCost}</h3>
+            </div>
+            <div class="legend_box">
+                <h3 class="text">${card.legend}</h3>
+            </div>
+        </div>
+        `
+    });
+}
+
+export {displayCard, displayRewardCard};
--- src/UI/screenDisplay.js
+++ src/UI/screenDisplay.js
@@ -3,9 +3,10 @@ export default function screenDisplay(state) {
         case "endScreen": // Change the div's styling to display the logs in the middle of the screen in a larger font
             document.querySelector(".top_bit").setAttribute("style", "flex-direction:row; background:black"); // I use .setAttribute instead of .style because flex-drection doesn't work with .style
             document.querySelector("#enemy").style.display = "none";
-            document.querySelector(".logs").setAttribute("style", "font-size:40px;width:100%;position:inherit;flex-direction:column;align-items:flex-start;");                
+            document.querySelector(".logs").setAttribute("style", "display: flex;top: 20%;left: 40%;font-size: 40px;position: absolute;align-items: flex-start;flex-direction: column;"); 
             document.querySelector("#itemRewardLog").setAttribute("style", "display:inline;")
             document.querySelector("#cardRewardLog").setAttribute("style", "display:inline;")
+            document.querySelector("#card_Reward_Visual").setAttribute("style", "display:flex;")
             break;
         case "restartScreen": // Change the div's styling to undo endScreen changes
             document.querySelector(".top_bit").setAttribute("style", "flex-direction:column; background-image: url('../src/images/Background.jpeg'); flex-direction: column; background-repeat: no-repeat; background-size: 100%; align-items: center;");
@@ -13,6 +14,7 @@ export default function screenDisplay(state) {
             document.querySelector(".logs").setAttribute("style", "font-size:24px;position:absolute");
             document.querySelector("#itemRewardLog").setAttribute("style", "display:none")
             document.querySelector("#cardRewardLog").setAttribute("style", "display:none")
-
+            document.querySelector("#card_Reward_Visual").setAttribute("style", "display:none;")
+            break;
     }
 }
