857d21438fde99e4a8159e51d631ccde8885bae3
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-08 20:28:40 +0100

    Add files via upload

--- src/UI/enemyStats.js
+++ src/UI/enemyStats.js
@@ -1,19 +1,21 @@
 
 const displayEnemyStatsHTML = (game) => {
 
-  
+
 
   const enemyStatsDiv = document.getElementById("enemy")
   enemyStatsDiv.innerHTML = ""
 
   game.enemies.forEach((enemy, index) => {
+
     enemyStatsDiv.innerHTML +=`
     <div class="enemy_div ${enemy.type}" id="enemy_${index}">
       <div id="stats">
         <h3>${enemy.name}'s Stats</h3>
         <p>type: ${enemy.type}</p>
         <p>Health: ${enemy.health}</p>
-        <div class="health_bar_container">
+        <div class="health_Bar_Container">
+          <p>${enemy.health} / ${enemy.spawnHealth} </p>
           <div class="health_Bar red"></div>
           <div class="health_Bar green"></div>
         </div>
@@ -24,6 +26,9 @@ const displayEnemyStatsHTML = (game) => {
     </div>
 
   `
+
+  document.querySelector(".health_Bar.green").style.width = enemy.health * 100 / enemy.spawnHealth + "%";
+
   });
   }
 
