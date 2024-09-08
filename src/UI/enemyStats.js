89a21e0c9ae37706f7fec0b17cc2bc23a670eee5
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-08 20:09:31 +0100

    Add files via upload

--- src/UI/enemyStats.js
+++ src/UI/enemyStats.js
@@ -1,24 +1,30 @@
 
 const displayEnemyStatsHTML = (game) => {
-    const enemyStatsDiv = document.getElementById("enemy")
-    enemyStatsDiv.innerHTML = ""
 
-    game.enemies.forEach((enemy, index) => {
-      enemyStatsDiv.innerHTML +=`
-      <div class="enemy_div ${enemy.type}" id="enemy_${index}">
-        <div id="stats">
-          <h3>${enemy.name}'s Stats</h3>
-          <p>type: ${enemy.type}</p>
-          <p>Health: ${enemy.health}</p>
-          <div class="health_Bar"></div>
-          <p>Damage: ${enemy.damage}</p>
-          <p>Next move: ${enemy.nextMove.move}<p>
+  
+
+  const enemyStatsDiv = document.getElementById("enemy")
+  enemyStatsDiv.innerHTML = ""
+
+  game.enemies.forEach((enemy, index) => {
+    enemyStatsDiv.innerHTML +=`
+    <div class="enemy_div ${enemy.type}" id="enemy_${index}">
+      <div id="stats">
+        <h3>${enemy.name}'s Stats</h3>
+        <p>type: ${enemy.type}</p>
+        <p>Health: ${enemy.health}</p>
+        <div class="health_bar_container">
+          <div class="health_Bar red"></div>
+          <div class="health_Bar green"></div>
         </div>
-        <img src= ${enemy.imgSrc} alt="img"/>
+        <p>Damage: ${enemy.damage}</p>
+        <p>Next move: ${enemy.nextMove.move}<p>
       </div>
+      <img src= ${enemy.imgSrc} alt="img"/>
+    </div>
 
-    `
-    });
+  `
+  });
   }
 
 export default displayEnemyStatsHTML;
