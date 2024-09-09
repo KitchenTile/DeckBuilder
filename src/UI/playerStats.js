d6bcd1fb68dc278c960cde611fd55d6ac7336b98
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-09 19:01:35 +0100

    Add files via upload

--- src/UI/playerStats.js
+++ src/UI/playerStats.js
@@ -4,12 +4,25 @@ const displayPlayerStatsHTML = (game) => {
   playerStats.innerHTML = `
     <div>
       <h3>${game.player.name}'s stats</h3>
-      <p>Health: ${game.player.health}/${game.player.maxHealth}</p>
+      <div class="health_Row">
+        <p>Health: </p>
+        <div class="health_Bar_Container">
+          <p class="health_Info">${game.player.health} / ${game.player.maxHealth} </p>
+          <div class="health_Bar red"></div>
+          <div class="health_Bar green" id="player_healthbar"></div>
+        </div>
+      </div>
       <p>Armor: ${game.player.armor}</p>
       <p>Energy: ${game.player.energy}/${game.player.startingEnergy}</p>
     </div>
     <img src= ${game.player.imgSrc} alt="img"/>
   `
+
+  const healthBar = document.getElementById(`player_healthbar`);
+
+  const barPercentage = game.player.health * 100 / game.player.maxHealth + "%";
+  
+  healthBar.style.width = barPercentage;
 }
 
 export default displayPlayerStatsHTML;
