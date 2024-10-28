04ae91fc7d966df20ea558d5636e82c594f3831b
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-10-28 12:24:45 +0000

    Add files via upload

--- src/UI/navBar.js
+++ src/UI/navBar.js
@@ -2,7 +2,7 @@ const navBar = () => {
     // Modify the navBar div to get as many links as we want retrieved from the list below.
 
     const navBarDiv = document.querySelector(".navBar");
-    const navBarData = [{name: "HOME"},{name: "SCORE BOARD"}];
+    const navBarData = [{name: "HOME"},{name: "SCORE BOARD"}, {name: "INSTRUCTIONS"}];
 
     navBarData.forEach((link, index) => {
         navBarDiv.innerHTML += `
